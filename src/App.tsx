6e57f74664f7e323fb644cd7a047e5dd771d3762
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-03-27 23:18:21 -0300

    feat: installing shadcn/ui :rocket:

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from './components/ui/button'
+import './global.css'
+
 export const App = () => {
-  return(
-    <div>
-      <h1>Hello</h1>
+  return (
+    <div className='flex justify-center items-center h-screen'>
+      <Button>
+        Enviar
+      </Button>
     </div>
   )
 }
